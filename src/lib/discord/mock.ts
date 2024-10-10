398076d6cf375a134e4dc5465d970865b9cb9a23
Carlos Azaustre <cazaustre@gmail.com>
2024-10-10 12:35:55 +0200

    test: :test_tube: add utils lib missing tests

--- src/lib/discord/mock.ts
+++ src/lib/discord/mock.ts
@@ -2,6 +2,17 @@ import { Question } from "@/types";
 import { DiscordApi } from "./types";
 import mockData from "../../mocks/discordData.json";
 
+/**
+ * Fetches Mock Discord posts and divides them into active and past posts.
+ *
+ * This function simulates an asynchronous fetch operation by waiting for 500 milliseconds.
+ * It then processes mock data to separate the posts into two categories:
+ * active posts and past posts.
+ *
+ * @returns An object containing two arrays:
+ * - `activePosts`: The first half of the posts.
+ * - `pastPosts`: The second half of the posts.
+ */
 export const fetchDiscordPosts: DiscordApi["fetchDiscordPosts"] = async () => {
   await new Promise((resolve) => setTimeout(resolve, 500));
 
@@ -15,6 +26,17 @@ export const fetchDiscordPosts: DiscordApi["fetchDiscordPosts"] = async () => {
   };
 };
 
+/**
+ * Fetches a Mock Discord thread by its ID.
+ *
+ * This function simulates a delay of 300 milliseconds to mimic an asynchronous
+ * API call. It searches for a thread within the `mockData.posts` array that matches
+ * the provided `threadId`. If a matching thread is found, it returns a deep copy
+ * of the thread object. If no matching thread is found, it returns `null`.
+ *
+ * @param threadId - The ID of the Discord thread to fetch.
+ * @returns A promise that resolves to the thread object if found, or `null` if not found.
+ */
 export const fetchDiscordThread: DiscordApi["fetchDiscordThread"] = async (
   threadId: string
 ) => {
